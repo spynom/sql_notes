c3769bdbac57840a3e623f5046cdcb88be17e591
Mohit Kumar <mkr9395@gmail.com>
2023-09-15 00:39:17 +0530

    any_all

--- hacker_rank.sql
+++ hacker_rank.sql
@@ -9378,4 +9378,35 @@ row_number() over(order by Actor) as rn_4
 from cte) as t4
 ON cte.rn=t4.rn_4
 )
-select * from cte2;
+select * from cte2
+where Professor!='zzz';
+
+
+
+-- Create the table
+-- CREATE TABLE BST (
+--     N INT,
+--     P INT
+-- );
+
+-- Insert values into the table-- 
+-- INSERT INTO BST (N, P)
+-- VALUES
+-- (1, 2),
+-- (3, 2),
+-- (5, 6),
+-- (7, 6),
+-- (2, 4),
+-- (6, 4),
+-- (4, 15),
+-- (8, 9),
+-- (10, 9),
+-- (12, 13),
+-- (14, 13),
+-- (9, 11),
+-- (13, 11),
+-- (11, 15),
+-- (15, NULL);
+
+
+
