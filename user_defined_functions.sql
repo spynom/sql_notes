da515ede94fa07d037551a098d0df144222c21a1
Mohit Kumar <mkr9395@gmail.com>
2023-11-09 00:02:27 +0530

    stored_p

--- user_defined_functions.sql
+++ user_defined_functions.sql
@@ -18,3 +18,25 @@ WHERE Pregnancies = 10;
 
 select *,preg_age(10) from diab;
 
+select * from diab;
+
+select preg_out(0,1) from diab;
+
+select factorial(-2);
+
+select GetNthFibonacciNumber(5);
+
+select BMI*1000 from diab
+where BloodPressure = 72 AND Insulin = 0 AND Age = 50;
+
+select * from diab;
+
+
+
+CALL glucose(50);
+
+CALL update_BMI(72, 0, 50);
+
+CALL generate_blood_glucose_report();
+
+CALL preg_glucose(2);
